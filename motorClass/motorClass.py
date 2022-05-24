c1ceda2eafba07f672aeb01d86a21bab35dd46d1
TestTube <testtube@vega.com>
2022-05-24 13:27:11 -0700

    free pins added

--- motorClass/motorClass.py
+++ motorClass/motorClass.py
@@ -51,6 +51,7 @@ class StepMotor:
         
 if __name__ == '__main__':
     stepM = StepMotor(4, 17, 23, 24)
-    stepM.forward(0.002, 1024)
-    stepM.backward(0.002, 1024)
+    stepM.forward(0.001, 512)
+    stepM.backward(0.001, 512)
+    GPIO.cleanup()
 
